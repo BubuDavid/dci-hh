505cb4d8a6dd7d1344590b6342a61714ac129ee5
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-11-14 16:37:39 -0600

    🍟 [ADD] The header in the main app 🍟

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from '../Header/Header'
+import Stars from '../Stars/Stars'
+import './App.css'
 
 function App() {
 	return (
-		<div>Hello World</div>
+		<>
+			<Stars />
+			<div id='app'>
+				<Header />
+
+			</div>
+		</>
 	)
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+	return (
+		<h1 className='Header NeonText NeonBorder'>
+			Hacedor de Horarios-Inador!
+		</h1>
+	)
+}
+
+export default Header
