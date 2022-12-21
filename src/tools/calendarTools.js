57a5be4e705649fe8ffef3eb777e1db5b0896b41
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-12-21 15:23:49 -0600

    🚩 [FIX] Mobile View 🚩

--- src/tools/calendarTools.js
+++ src/tools/calendarTools.js
@@ -22,6 +22,7 @@ function generateTimeSlots(schedule) {
 		let nMaxDays = 6
 		for (let i = 1; i <= nMaxDays; i++) {
 			if (!subject[`DAY/TIME/ROOM${i}`]) break
+			if (!subject[`TIME${i}`]) break
 
 			let lowTime = subject[`TIME${i}`].split('-')[0]
 			let highTime = subject[`TIME${i}`].split('-')[1]
@@ -29,11 +30,6 @@ function generateTimeSlots(schedule) {
 			if (highTime > upperTime) upperTime = highTime
 
 		}
-		// for (const timeZone of subject.time_zones) {
-		// 	let timeRange = timeZone.time_range
-		// 	if (timeRange.low < lowerTime) lowerTime = timeRange.low
-		// 	if (timeRange.high > upperTime) upperTime = timeRange.high
-		// }
 	}
 
 	for (let t = lowerTime; t < upperTime; t++) {
@@ -170,6 +166,7 @@ function getEventsForCalendar(schedule, timeSlots, nColumns) {
 
 		for (let dayIndex = 1; dayIndex < nMaxDays; dayIndex++) {
 			if (!subject[`DAY/TIME/ROOM${dayIndex}`]) break
+			if (!subject[`TIME${dayIndex}`] || !subject[`DAY${dayIndex}`]) break
 
 			let tempSubject = {}
 			tempSubject.NAME = getName(subject.NAME)
@@ -200,37 +197,6 @@ function getEventsForCalendar(schedule, timeSlots, nColumns) {
 			// -------- Add to array -------- //
 			events.push(tempSubject)
 		}
-
-		// for (const time of subject.time_zones) {
-		// 	let tempSubject = {}
-		// 	// -------- Get info -------- //
-		// 	tempSubject.name = getName(subject.name)
-		// 	tempSubject.group = subject.group
-		// 	tempSubject.professor1 = getProfessor(subject.professor1)
-		// 	tempSubject.professor2 = getProfessor(subject.professor2)
-		// 	if (subject.professor3) tempSubject.professor3 = getProfessor(subject.professor3)
-		// 	const [
-		// 		newTime,
-		// 		newRoom
-		// 	] = getDayDescription(subject[`day${dayIndex}`])
-		// 	tempSubject.time = newTime
-		// 	tempSubject.room = newRoom
-		// 	dayIndex++
-		// 	// -------- Get position -------- //
-		// 	// Get column
-		// 	tempSubject.column = getColumn(time)
-		// 	// Get row
-		// 	tempSubject.row = getRow(time, timeSlots)
-		// 	// -------- Get size -------- //
-		// 	// Get height
-		// 	tempSubject.height = `${getHeight(time)}px`
-		// 	// Get width
-		// 	tempSubject.width = `${getWidth(tempSubject.column, nColumns)}%`
-		// 	// Get color
-		// 	tempSubject.color = color
-		// 	// -------- Add to array -------- //
-		// 	events.push(tempSubject)
-		// }
 	}
 
 	return events
